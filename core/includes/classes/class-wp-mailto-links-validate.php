8934321fbbbac2541b2754d69a37458abea33a35
Jannis Thümmig <jannis@thuemmig.com>
2019-10-26 11:25:01 +0100

    Fix issue with zeros getting stripped away from email using javascript escape method

--- core/includes/classes/class-wp-mailto-links-validate.php
+++ core/includes/classes/class-wp-mailto-links-validate.php
@@ -411,7 +411,7 @@ class WP_Mailto_Links_Validate{
 
               foreach( $split as $c ) {
                 // preg split will return empty first and last characters, check for them and ignore
-                if( ! empty( $c ) ) {
+                if( ! empty( $c ) || $c === '0' ) {
                   $out .= '%' . dechex( ord( $c ) );
                 }
               }
