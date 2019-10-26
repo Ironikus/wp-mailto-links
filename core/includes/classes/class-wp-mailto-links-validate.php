d149ec0703e7f37aadd9089825c0fa41f9bcfb51
Jannis Thümmig <jannis@thuemmig.com>
2019-10-26 11:20:43 +0100

    validate all preg matches

--- core/includes/classes/class-wp-mailto-links-validate.php
+++ core/includes/classes/class-wp-mailto-links-validate.php
@@ -298,7 +298,7 @@ class WP_Mailto_Links_Validate{
         foreach( $soft_attributes as $ident => $regex ){
 
             $array = array();
-            preg_match( $regex, $content, $array ) ;
+            preg_match_all( $regex, $content, $array ) ;
 
             foreach( $array as $single ){
                 $content = str_replace( $single, $this->filter_plain_emails( $single, null, $protection_method, false ), $content );
